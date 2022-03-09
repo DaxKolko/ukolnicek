cd7cf52d637a795a87295c4aa39d8ee899a09ece
Dan <danko.kolko@gmail.com>
2022-03-09 18:50:09 +0100

    fix error

--- script.js
+++ script.js
@@ -1,62 +1,70 @@
-const taskcontainer = document.querySelector(".container");
+const taskContainer = document.querySelector(".container");
 const input = document.querySelector("#task-to-add");
 const button = document.querySelector(".button");
-let newtasktext = "";
-console.log(taskcontainer);
-const completeTask = document.querySelector("#completed");
-let completedtasknumber = 0;
-const totaltask = document.querySelector("#total");
-let totaltaskmumber = 0;
-const changeTaskStatus = (event) => {
-  //event.target.classList.contains("done")) {
+const completedTasks = document.querySelector("#completed");
+const totalTasks = document.querySelector("#total");
+console.log(totalTasks);
+let newTaskText = "";
+let completedTasksNumber = 0;
+let totalTasksNumber = localStorage.getItem("tasksNumber") || 0;
+totalTasks.innerHTML = totalTasksNumber;
+
+function changeTaskStatus(event) {
+  // event.target.classList.toggle("done");
   if (event.target.classList.contains("done")) {
     event.target.classList.remove("done");
-    completedtasknumber--;
-    totaltaskmumber--;
+    completedTasksNumber--;
+    totalTasksNumber--;
+    localStorage.setItem("tasksNumber", totalTasksNumber);
   } else {
     event.target.classList.add("done");
-    completedtasknumber++;
-    totaltaskmumber++;
+    completedTasksNumber++;
+    totalTasksNumber++;
+    localStorage.setItem("tasksNumber", totalTasksNumber);
   }
-  completeTask.innerHTML = completedtasknumber;
-};
-const removetask = (event) => {
+  completedTasks.innerHTML = completedTasksNumber;
+  totalTasks.innerHTML = totalTasksNumber;
+}
+
+const removeTask = (event) => {
   event.target.parentElement.parentElement.remove();
 };
 
-const createnewtask = () => {
+const createNewTask = () => {
   const task = document.createElement("div");
-  task.innerHTML = `<div class="task ">  
-        <div class="checkbox not-done"></div>
-        <p>Úkol</p>
-        <i class="bin far fa-trash-alt"></i>
-        </div>`;
-  taskcontainer.appendChild(task);
-  task.querySelector("p").innerHTML = newtasktext;
+  task.innerHTML = `<div class="task">  
+  <div class="checkbox not-done"></div>
+  <p>Úkol</p>
+  <i class="bin far fa-trash-alt"></i>
+</div>`;
+  taskContainer.appendChild(task);
+  task.querySelector("p").innerHTML = newTaskText;
   const checkbox = task.querySelector(".checkbox");
   const bin = task.querySelector(".bin");
-  checkbox.onclick = (e) => changeTaskStatus(e);
-  bin.onclick = () => {
-    if (confirm("Opravdu chceš smazat úkol ?")) {
-      removetask(event);
+  checkbox.onclick = (event) => changeTaskStatus(event);
+  bin.onclick = (event) => {
+    if (confirm("Opravdu chceš úkol smazat?")) {
+      removeTask(event);
     }
   };
   input.value = "";
 };
+
 button.onclick = () => {
-  if (input.value !== "") {
-    createnewtask();
+  if (newTaskText !== "") {
+    createNewTask();
   } else {
-    alert("Vyplň políčko");
+    alert("Vyplň políčko!");
   }
 };
+
 input.onkeyup = (e) => {
-  newtasktext = e.target.value;
+  newTaskText = e.target.value;
   if (e.keyCode === 13) {
-    if (Ne !== "") {
-      createnewtask();
+    if (newTaskText !== "") {
+      createNewTask();
     } else {
-      alert("Vyplň políčko");
+      alert("Vyplň políčko!");
     }
   }
 };
